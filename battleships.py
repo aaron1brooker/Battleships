b671cd9b37db56e8eaeb3144e83610f1ed7a0237
aaron1brooker <54953569+aaron1brooker@users.noreply.github.com>
2022-12-02 10:51:31 +0000

    User can populate grid and basic grid ui

--- battleships.py
+++ battleships.py
@@ -1,15 +1,50 @@
+import os
+import logging
+import time
+from colorama import Fore
+
 from svc.game_setup import GameSetup
-from src.create_grid import PlayerGrids
+from src.player_grid import PlayerGrid
+from util.util import Util
+from util.exceptions import UserError
+
+# Logging output will be put into a txt file to help debugging
+logging.basicConfig(filename="battleships_logs.txt", level=logging.INFO)
+
 
 def start_game():
-	"""This will start battleships"""
-	game_config = GameSetup()
-	board, boats = game_config.get_game_config()
+    """This will start battleships"""
 
-	print(isinstance(board["y"], int))
-	player = PlayerGrids(board["x"], board["y"], boats)
-	player.display_remaining_boats()
-	player.insert_boat(("C",0), 3, "left")
-	
+    # Setup game
+    try:
+        game_config = GameSetup()
+        board, boats = game_config.get_game_config()
+        player = PlayerGrid(board["x"], board["y"], boats)
+    except UserError as e:
+        print(e)
+        print("Reconfigure configuration file and try again")
+        return
+
+    while player.is_boats():
+        os.system("clear")  # clears the console so we can put the updated grid in
+        player.display_board()
+        player.display_remaining_boats()
+        choice = input(
+            f"{Fore.BLUE}\nEnter the boat, postion and direction. E.g. carrier E1 down: {Fore.WHITE}"
+        )
+        try:
+            boat, pos, direction = Util.seperate_choice(choice)
+            if not player.place_boat(pos, boat, direction):
+                print(
+                    f"{Fore.RED}It was not possible to position this boat. Please try again{Fore.WHITE}"
+                )
+                time.sleep(1.5)
+        except UserError as e:
+            print(f"{Fore.RED}{e}. Please try again{Fore.WHITE}")
+            time.sleep(1.5)
+
+    os.system("clear")
+    player.display_board()
+
 
 start_game()
---
+++ src/player_grid.py
@@ -0,0 +1,145 @@
+import logging
+from colorama import Fore
+from typing import Dict, Tuple
+
+from util.constants import DIRECTION
+from util.exceptions import UserError
+from util.util import Util
+
+
+class PlayerGrid:
+    """Class that controls players actions and their grids"""
+
+    def __init__(self, x_length: int, y_length: int, boats: Dict) -> None:
+        """Constructor"""
+
+        self.__grid = [0] * x_length * y_length
+        self.__x_length = x_length
+        self.__y_length = y_length
+        self.__player_boats = boats
+
+    def __insert_boat(
+        self, pos: Tuple[str, int], boat_len: int, direction: int
+    ) -> bool:
+        """Inserts the boat into the grid and returns a status boolean if successful"""
+
+        x, y = pos
+        x_int = ord(x.upper()) - 65
+        y -= 1
+
+        if not 0 <= x_int < self.__x_length:
+            logging.warning(f"{x} is not a valid x value")
+            return False
+
+        if not 0 <= y < self.__y_length:
+            logging.warning(f"{y} is not a valid y value")
+            return False
+
+        index_row = y * self.__y_length  # this is the furthest left index of the row
+        if self.__grid[index_row + x_int] == 1:
+            logging.warning(
+                "Cannot place boat. Position is occupied by an object already"
+            )
+            return False
+        positions = [index_row + x_int]
+
+        for ii in range(boat_len - 1):
+            # 'direction_sum' determines how much it needs to move by to place the next 'part'
+            # of the ship
+            if direction == 1 or direction == 3:
+                direction_sum = -self.__x_length if direction == 1 else self.__x_length
+                # Check if it is within the grids dimensions
+                if positions[ii] + direction_sum < 0 or positions[
+                    ii
+                ] + direction_sum > len(self.__grid):
+                    logging.warning("Out of the grids range")
+                    return False
+            else:
+                # We cam safely assume that only direction 2 and 4 will be passed
+                direction_sum = 1 if direction == 2 else -1
+                furthest_right_index = index_row + self.__x_length - 1
+
+                # Without this check it would go on to the previous/next grid row
+                if ((positions[ii] + direction_sum) > furthest_right_index) or (
+                    (positions[ii] + direction_sum) < index_row
+                ):
+                    logging.warning("Out of the grids range")
+                    return False
+
+            # Check if position is currently occupied
+            if self.__grid[positions[ii] + direction_sum] == 1:
+                logging.warning(
+                    "Cannot place boat. Position is occupied by an object already"
+                )
+                return False
+
+            positions.append(positions[ii] + direction_sum)
+
+        for position in positions:
+            self.__grid[position] = 1
+
+        return True
+
+    def place_boat(self, pos: Tuple[str, int], boat: str, direction: str) -> bool:
+        """Supplies and executes __insert_boat with boat data and validates direction"""
+
+        direction = DIRECTION.get(direction.lower())
+        boat_len = self.__player_boats.get(boat.lower())
+
+        if not (direction and boat_len):
+            logging.warning(
+                f"Unable to place the boat due to provided parameters: {direction} {boat}"
+            )
+            return False
+
+        if self.__insert_boat(pos, boat_len, direction):
+            self.__player_boats.pop(boat.lower())
+            return True
+
+        # A warning message will have been provided by __insert_boat if it failed
+        return False
+
+    def is_boats(self) -> bool:
+        """Returns true if there are boats still left"""
+
+        if len(self.__player_boats) == 0:
+            return False
+
+        return True
+
+    def display_remaining_boats(self) -> None:
+        """Prints the remaining boats to place onto the grid"""
+
+        for boat in self.__player_boats:
+            print(Fore.WHITE + boat)
+
+    def display_board(self) -> None:
+        """Prints the board in its current state"""
+
+        column_spacing = len(str(self.__y_length)) + 1
+
+        x_axis_label = []
+        x_axis_label.append(f"{Util.add_spaces('', column_spacing)}|")
+        for ii in range(self.__x_length):
+            # Print x axis labels
+            if ii + 65 > 90:
+                # For now, we do not allow columns greater than 26
+                raise UserError("X axis is not able to exceed for that 26 columns")
+            x_axis_label.append(chr(ii + 65))
+        print(" ".join(x_axis_label))
+
+        # Print dashes under headers
+        x_axis_dashes = []
+        x_axis_dashes.append(f"{Util.add_spaces('', column_spacing)}|")
+        for ii in range(len(x_axis_label) - len(x_axis_dashes)):
+            x_axis_dashes.append("-")
+        print(" ".join(x_axis_dashes))
+
+        pos = 0
+        for y_label in range(self.__y_length):
+            row_data = []
+            row_data.append(f"{Util.add_spaces(str(y_label + 1), column_spacing)}|")
+            for ii in range(self.__x_length):
+                row_data.append(str(self.__grid[pos]))
+                pos += 1
+            print(" ".join(row_data))
