5662f80570e0cfdfb08558e46e6e12d6011e39a9
aaron1brooker <54953569+aaron1brooker@users.noreply.github.com>
2022-12-18 20:52:06 +0000

    Stage 5 - Hidden Mines Battleships (#5)
    Co-authored-by: Brooker <aaron.brooker@ada.ac.uk>

--- battleships.py
+++ battleships.py
@@ -6,8 +6,10 @@ from colorama import Fore
 from svc.game_setup import GameSetup
 from src.grid_components.automated_grid import AutomatedGrid
 from src.grid_components.salvo_grid import SalvoGrid
+from src.grid_components.mines_grid import MinesGrid
 from src.games.traditional_battleships import TraditionalBattleships
 from src.games.salvo_battleship import SalvoBattleships
+from src.games.hidden_mines import HiddenMines
 
 # Logging output will be put into a txt file to help debugging
 logging.basicConfig(filename="logs.txt", level=logging.INFO)
@@ -23,7 +25,7 @@ class Battleships:
         game_config = GameSetup()
         self.__board, self.__boats = game_config.get_game_config()
 
-    def __battleships(self):
+    def __battleships(self, is_computer_p1: bool, is_computer_p2: bool) -> None:
         """Play Battleships (player vs computer)"""
 
         # Intialise Players
@@ -31,21 +33,12 @@ class Battleships:
         player2 = AutomatedGrid(self.__board["x"], self.__board["y"], self.__boats)
 
         # Play the game
-        battleships = TraditionalBattleships(player1, player2, False, True)
+        battleships = TraditionalBattleships(
+            player1, player2, is_computer_p1, is_computer_p2
+        )
         battleships.play_game()
 
-    def __battleships_two_player(self):
-        """Play Battleships (Two Player)"""
-
-        # Intialise Players
-        player1 = AutomatedGrid(self.__board["x"], self.__board["y"], self.__boats)
-        player2 = AutomatedGrid(self.__board["x"], self.__board["y"], self.__boats)
-
-        # Play the game
-        battleships = TraditionalBattleships(player1, player2, False, False)
-        battleships.play_game()
-
-    def __salvo_battleships(self):
+    def __salvo_battleships(self, is_computer: bool) -> None:
         """Play Salvo Battleships (player vs computer)"""
 
         # Intialise Players
@@ -53,18 +46,18 @@ class Battleships:
         player2 = SalvoGrid(self.__board["x"], self.__board["y"], self.__boats)
 
         # Play the game
-        salvo_battleships = SalvoBattleships(player1, player2, False, True)
+        salvo_battleships = SalvoBattleships(player1, player2, False, is_computer)
         salvo_battleships.play_game()
 
-    def __salvo_battleships_two_player(self):
-        """Play Salvo Battleships (Two Player)"""
+    def __hidden_mines_battleship(self, is_computer: bool) -> None:
+        "Play Hidden Mines Battleships"
 
         # Intialise Players
-        player1 = SalvoGrid(self.__board["x"], self.__board["y"], self.__boats)
-        player2 = SalvoGrid(self.__board["x"], self.__board["y"], self.__boats)
+        player1 = MinesGrid(self.__board["x"], self.__board["y"], self.__boats)
+        player2 = MinesGrid(self.__board["x"], self.__board["y"], self.__boats)
 
         # Play the game
-        salvo_battleships = SalvoBattleships(player1, player2, False, False)
+        salvo_battleships = HiddenMines(player1, player2, False, is_computer)
         salvo_battleships.play_game()
 
     def menu(self):
@@ -73,24 +66,33 @@ class Battleships:
         os.system("cls")
 
         menu_screen = (
-            f"{Fore.BLUE}MENU:\n{Fore.WHITE}1) Battleships (player v computer)\n2) Battleships (two player)\n3) Salvo Battleships (player v computer)\n"
-            + "4) Salvo Battleships (two player)\n5) Quit\n"
+            f"{Fore.BLUE}MENU:\n{Fore.WHITE}1) Battleships (Player v Computer)\n2) Battleships (Two player)\n3) Salvo Battleships (Player v Computer)\n"
+            + "4) Salvo Battleships (Two player)\n5) Hidden Mines Battleships (Player v Computer)\n"
+            + "6) Hidden Mines Battleships (Two player)\n7) Battleships (Computer v Computer)\n8) Quit\n"
         )
         user_choice = input(menu_screen)
 
         if user_choice == "1":
-            self.__battleships()
+            self.__battleships(False, True)
             return
         elif user_choice == "2":
-            self.__battleships_two_player()
+            self.__battleships(False, False)
             return
         elif user_choice == "3":
-            self.__salvo_battleships()
+            self.__salvo_battleships(True)
             return
         elif user_choice == "4":
-            self.__salvo_battleships_two_player()
+            self.__salvo_battleships(False)
             return
         elif user_choice == "5":
+            self.__hidden_mines_battleship(True)
+            return
+        elif user_choice == "6":
+            self.__hidden_mines_battleship(False)
+        elif user_choice == "7":
+            self.__battleships(True, True)
+            return
+        elif user_choice == "8":
             print(f"{Fore.BLUE}Goodbye!!{Fore.WHITE}")
             return
         else:
